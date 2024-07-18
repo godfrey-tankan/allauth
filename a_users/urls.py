a5cb824abdf0731f9683397490347c8e5c001eb9
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-18 11:04:14 +0200

    chore: Update profile settings page to enable email editing
    This commit updates the profile_settings.html template to enable editing of the user's email address. It adds the necessary attributes and classes to the email edit link, and includes the hx-target, hx-swap, and hx-get attributes for htmx functionality. This change allows users to easily edit their email address directly on the profile settings page.

--- a_users/urls.py
+++ a_users/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('edit/', profile_edit_view, name="profile-edit"),
     path('onboarding/', profile_edit_view, name="profile-onboarding"),
     path('settings/', profile_settings_view, name="profile-settings"),
-    # path('emailchange/', profile_emailchange, name="profile-emailchange"),
+    path('emailchange/', profile_emailchange, name="profile-emailchange"),
     # path('emailverify/', profile_emailverify, name="profile-emailverify"),
     # path('delete/', profile_delete_view, name="profile-delete"),
 ]
