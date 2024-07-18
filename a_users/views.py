a8468501e7c6c34060f5c250b9c3d98488f89033
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-18 10:03:20 +0200

    Refactor a_users/forms.py and templates/includes/header.html

--- a_users/views.py
+++ a_users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
-from allauth.account.utils import send_email_confirmation
+# from allauth.account.utils import send_email_confirmation
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
@@ -28,10 +28,11 @@ def profile_edit_view(request):
             form.save()
             return redirect('profile')
         
-    if request.path == reverse('profile-onboarding'):
-        onboarding = True
-    else:
-        onboarding = False
+    # if request.path == reverse('profile-onboarding'):
+    #     onboarding = True
+    # else:
+    #     onboarding = False
+    onboarding = False
       
     return render(request, 'a_users/profile_edit.html', { 'form':form, 'onboarding':onboarding })
 
@@ -41,52 +42,52 @@ def profile_settings_view(request):
     return render(request, 'a_users/profile_settings.html')
 
 
-@login_required
-def profile_emailchange(request):
+# @login_required
+# def profile_emailchange(request):
     
-    if request.htmx:
-        form = EmailForm(instance=request.user)
-        return render(request, 'partials/email_form.html', {'form':form})
+#     if request.htmx:
+#         form = EmailForm(instance=request.user)
+#         return render(request, 'partials/email_form.html', {'form':form})
     
-    if request.method == 'POST':
-        form = EmailForm(request.POST, instance=request.user)
+#     if request.method == 'POST':
+#         form = EmailForm(request.POST, instance=request.user)
 
-        if form.is_valid():
+#         if form.is_valid():
             
-            # Check if the email already exists
-            email = form.cleaned_data['email']
-            if User.objects.filter(email=email).exclude(id=request.user.id).exists():
-                messages.warning(request, f'{email} is already in use.')
-                return redirect('profile-settings')
+#             # Check if the email already exists
+#             email = form.cleaned_data['email']
+#             if User.objects.filter(email=email).exclude(id=request.user.id).exists():
+#                 messages.warning(request, f'{email} is already in use.')
+#                 return redirect('profile-settings')
             
-            form.save() 
+#             form.save() 
             
-            # Then Signal updates emailaddress and set verified to False
+#             # Then Signal updates emailaddress and set verified to False
             
-            # Then send confirmation email 
-            send_email_confirmation(request, request.user)
+#             # Then send confirmation email 
+#             send_email_confirmation(request, request.user)
             
-            return redirect('profile-settings')
-        else:
-            messages.warning(request, 'Form not valid')
-            return redirect('profile-settings')
+#             return redirect('profile-settings')
+#         else:
+#             messages.warning(request, 'Form not valid')
+#             return redirect('profile-settings')
         
-    return redirect('home')
+#     return redirect('home')
 
 
-@login_required
-def profile_emailverify(request):
-    send_email_confirmation(request, request.user)
-    return redirect('profile-settings')
+# @login_required
+# def profile_emailverify(request):
+#     send_email_confirmation(request, request.user)
+#     return redirect('profile-settings')
 
 
-@login_required
-def profile_delete_view(request):
-    user = request.user
-    if request.method == "POST":
-        logout(request)
-        user.delete()
-        messages.success(request, 'Account deleted, what a pity')
-        return redirect('home')
+# @login_required
+# def profile_delete_view(request):
+#     user = request.user
+#     if request.method == "POST":
+#         logout(request)
+#         user.delete()
+#         messages.success(request, 'Account deleted, what a pity')
+#         return redirect('home')
     
-    return render(request, 'a_users/profile_delete.html')
+#     return render(request, 'a_users/profile_delete.html')
