26425496d581aa02659b56bda4d7d15a415ecb50
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-18 11:21:29 +0200

    chore: Update ALLOWED_HOSTS to allow all hosts

--- a_core/settings.py
+++ a_core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ww7(ub2l)1qjl6fzq(hp9%f=+xh6sf^j%##ja1z7qwt%+v71%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
