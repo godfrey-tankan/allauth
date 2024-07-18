4eb53d2b344b6bb0efe17fc44cce8c84bb32de88
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-18 10:10:04 +0200

    chore: Configure media handling and static files

--- a_core/settings.py
+++ a_core/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
