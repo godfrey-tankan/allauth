75394609c450ef0c545c695a997eb0c39e2b6e86
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-18 11:40:20 +0200

    chore: Update SECRET_KEY and DEBUG settings
    This commit updates the SECRET_KEY and DEBUG settings in the settings.py file. The SECRET_KEY has been changed to a new value, and DEBUG has been set to True. These changes are necessary for development purposes and should not be used in production.

--- a_core/settings.py
+++ a_core/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ww7(ub2l)1qjl6fzq(hp9%f=+xh6sf^j%##ja1z7qwt%+v71%a'
+SECRET_KEY = 'django-insecure-kk2(ub2l)1qjl6fzq(hp9%f=+xh6sf^j%##ja1z7qwt%+v71%a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
