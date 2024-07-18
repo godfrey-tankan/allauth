f38f5cca1d90336be78ad779c8fef00f01dac35a
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-07-18 09:03:15 +0200

    chore: Add static files configuration
    ```

--- a_core/settings.py
+++ a_core/settings.py
@@ -112,6 +112,9 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
