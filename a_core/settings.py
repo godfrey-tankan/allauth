375d7baac95b7378e2aeec3ca9c34a6195e44bb8
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-08-04 05:46:26 +0200

    chore: Update home_view to return JSON response
    This commit updates the home_view function in views.py to return a JSON response instead of rendering the 'home.html' template. It adds the necessary import statement for the JsonResponse class from django.http and modifies the return statement to return a JSON object with a 'message' key and value of 'Home!'. This change improves the functionality of the home page by returning data in a more structured format.

--- a_core/settings.py
+++ a_core/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'a_core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / '../../src'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- a_home/views.py
+++ a_home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def home_view(request):
-    return render(request, 'home.html')
+    return JsonResponse({'message': 'Home!'})
