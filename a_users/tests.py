e9e6ca898867ecbb933aac93f99895e3df1decdd
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-23 12:21:03 +0200

    chore: Add test for testMyFunc in a_users/tests.py

--- a_users/tests.py
+++ a_users/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+def testMyFunc(**params):
+    print(params)
+    return params.get("name")
+
+call_it = testMyFunc(name="tnqn", age=23,hobby="coding", country="Zim")
+print(call_it)
