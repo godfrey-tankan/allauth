d433aad5a24538cd474dd19472de20fbc9cc9f81
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-23 12:28:02 +0200

    Update home_view to require login

--- a_home/views.py
+++ a_home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+@login_required
 def home_view(request):
     return render(request, 'home.html')
