809e7c6b5b0c7cc7b79f9ae266bba00377e6c232
godfrey-tankan <godfreytantswakandeya@gmail.com>
2024-10-23 13:06:04 +0200

    Refactor home_view to redirect to landing page for unauthenticated users

--- a_home/views.py
+++ a_home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 # Create your views here.
-@login_required
+
 def home_view(request):
+    if not request.user.is_authenticated:
+        return render(request, 'landing.html')
     return render(request, 'home.html')
